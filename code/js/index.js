e907e7ae59e83d89ba8e5d09f8f57ccd1221383f
beatrice-yun <beatrice.yk.daniel@gmail.com>
2018-10-27 17:01:12 +0200

    all the html structure finished

---
+++ code/js/index.js
@@ -0,0 +1,24 @@
+// Function to delete matches
+function deleteMatch(e){
+
+}
+
+// Function to create a pronostic
+function createPronoInput(){
+
+}
+
+// Function to delete a match
+function createDeleteButton(){
+
+}
+
+// The function to generate a full match row
+function createNewItemRow(itemName, itemUnitPrice){
+
+}
+
+// Code initially included to associate buttons with their respective DOM functions
+window.onload = function(){
+
+}
